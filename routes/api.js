ed94511e9d8fdbe1c9fc05a6715805f9cf527044
zergity <zergity@gmail.com>
2021-06-17 14:57:53 +0700

    remove unused template module

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,8 @@
 var express = require("express");
 var stateRouter = require("./state");
-var authRouter = require("./auth");
-var bookRouter = require("./book");
-var farmRouter = require("./farm");
 
 var app = express();
 
 app.use("/state/", stateRouter);
-app.use("/auth/", authRouter);
-app.use("/book/", bookRouter);
-app.use("/farm/", farmRouter);
 
 module.exports = app;
